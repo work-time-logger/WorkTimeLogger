32ad953d3d160b46b3d1ee5c9037fb2580e2152b
Krystian Duma <git@krystian.duma.sh>
2019-05-10 03:01:17 +0200

    Enable model docblock generation

--- WorkTimeLoggerServer/app/Models/HardwareScanner.php
+++ WorkTimeLoggerServer/app/Models/HardwareScanner.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\HardwareScanner
+ *
+ * @property int $id
+ * @property string $api_token
+ * @property string $name
+ * @property int $is_active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner whereApiToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\HardwareScanner whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class HardwareScanner extends Model
 {
 
